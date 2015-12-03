3aef614efd15f171bebc617a857bed533e82e782
Serg09 <sergeyskumatov@aol.com>
2015-12-03 07:32:08 -0700

    Add whenever gen and cron for automated emails
    [#108834124]

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   def digest_email_update(user)
     @user = user
-      # attachments['Speed_OP_*'.last] = File.read('app/assets/files')
+    attachments['Speed OP.docx'] = File.read('app/assets/files/Speed OP.docx')
       mail(:to => 'sergey_skumatov@hotmail.com', :bcc => @user.email, :subject => "test mailing email", :from => "speedopdavinci@gmail.com") do
         # format.text(:content_type => "text/plain", :charset => "UTF-8", :content_transfer_encoding => "7bit")
     end
--- lib/tasks/email_tasks.rake
+++ lib/tasks/email_tasks.rake
@@ -23,10 +23,10 @@
 #   end
 #   end
 
-# namespace :send_email do
+# namespace :send_digest_email do
   desc 'send digest email'
   task send_digest_email: :environment do
     User.find_each do |user|
       UserMailer.digest_email_update(user).deliver_now
     end
-    end
+end
