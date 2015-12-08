7b3fd994a40b36bdc36af3117c761c025155d1b5
Serg09 <sergeyskumatov@aol.com>
2015-12-08 00:00:32 -0700

    commmit before rebate

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,16 +14,16 @@ class UserMailer < ActionMailer::Base
          subject: 'testing automated email service')
   end
 
-  def follow_up_email(email)
-    @user.email = email
-    # @url = 'http://speedop.com'
-    # attachments['Speed OP.docx'] = File.read('app/assets/files/Speed OP.docx')
-    # attachments.inline['666.txt'] = File.read('app/assets/files/666.txt')
-    # attachments.inline['octocat.png'] = File.read('app/assets/images/octocat.png')
-    mail(to: @user.email,
-         # bcc: 'info@speedop.com',
-         bcc: 'sergeyskumatov@aol.com',
-         subject: 'testing automated email service')
-  end
+  # def follow_up_email(email)
+  #   @user.email = email
+  #   # @url = 'http://speedop.com'
+  #   # attachments['Speed OP.docx'] = File.read('app/assets/files/Speed OP.docx')
+  #   # attachments.inline['666.txt'] = File.read('app/assets/files/666.txt')
+  #   # attachments.inline['octocat.png'] = File.read('app/assets/images/octocat.png')
+  #   mail(to: @user.email,
+  #        # bcc: 'info@speedop.com',
+  #        bcc: 'sergeyskumatov@aol.com',
+  #        subject: 'testing automated email service')
+  # end
 end
 
