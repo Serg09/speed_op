d0c90f272448ecedf652f1a8286b5ca03debfe32
Serg09 <sergeyskumatov@aol.com>
2015-12-08 00:00:32 -0700

    commmit before rebate

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host:'localhost:3000' }
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default_options = {from: 'speedopdavinci@gmail.com'}
@@ -28,8 +28,10 @@ Rails.application.configure do
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: 'speedopdavinci@gmail.com',
-    password: "Speedop_2015",
+    user_name: ENV["GMAIL_USERNAME"],
+    # user_name: 'speedopdavinci@gmail.com',
+    password: ENV["GMAIL_PASSWORD"],
+    # password: 'Speedop_2015',
     openssl_verify_mode: 'none'
   }
 
