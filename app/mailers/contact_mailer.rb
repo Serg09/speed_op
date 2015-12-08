12bb4e69b8106c0fe70d21e9e884993a3bcb9d81
Serg09 <sergeyskumatov@aol.com>
2015-12-08 12:54:14 -0700

    UserMailer. Does not have email information through API login. Must be fixed

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,9 +3,11 @@ class ContactMailer < ActionMailer::Base
 
   def welcome_email(contact)
     @contact = contact
-    @url = 'http://www.google.com'
+    @url = 'http://speedop.com'
     attachments['Speed OP.docx'] = File.read('app/assets/files/Speed OP.docx')
     attachments.inline['ie-1-1.gif'] = File.read('app/assets/images/ie-1-1.gif')
-    mail(to: @contact.email, bcc: 'sergey_skumatov@hotmail.com', subject: 'Testing the FORM_contact_mailer', reply_to: 'info@speedop.com')
+    mail(to: @contact.email,
+         bcc: 'sergey_skumatov@hotmail.com',
+         subject: 'Testing the FORM_contact_mailer')
   end
 end
